82972b98effe0e7746a3426f8bbcdaa7556b9048
AlexanderPalencia <mapg23@outlook.com>
2020-09-27 23:55:53 -0600

    adding lab 5

--- lab5/script.R
+++ lab5/script.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(lubridate)
 library(readxl)
 library(plyr)
+library(DescTools)
 
 eclipse_init <- dmy_hms('21-08-2017 18:26:40')
 synodic <- ddays(29) + dhours(12) + dminutes(44) + dseconds(3)
@@ -78,17 +79,50 @@ duration_call <- dfTotal %>%
 
 
 # Parte 3: Signo Zodiacal solo acepta como input la fecha en formato dmy
-zod_name <- c('Aries', 'Tauro', 'Geminis', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Escorpio', 'Sagitario', 'Capricornio', 'Acuario', 'Piscis')
-zod_day <- c('21', '21', '21', '22', '23', '23', '23', '23', '23', '22', '21', '19')
-zod_month <- c('3','4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2')
-zdf_df <- as.data.frame(zod_name, zod_day, zod_month)
+
+name_zod <- function(birth) {
+  valid_date <- dmy(birth)
+  signo <- Zodiac(valid_date)
+  return(signo)
+}
+a <- name_zod('23-10-1997')
+paste(' Tu signo es ', a)
+
+# No es preciso pero se hizo para demostrar que no es necesario una libreria
+# como funciona se selecciona el dia que se ingreso y se cuenta la cantidad de dias que pasaron
+# en base a esto se filtra en el dataset por un rango y se sabe que signo es
+zod_name <- c('Capricornio','Acuario', 'Piscis', 'Aries', 'Tauro', 'Geminis', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Escorpio', 'Sagitario', 'Capricornio')
+zod_day <- c('21', '50', '80', '110', '140', '170', '200', '230', '260', '290', '310', '340', '365')
+zod_day_fin <- c('0', '21', '51', '110', '140', '170', '200', '230', '260', '290', '310', '340', '365')
+zod_df <- data.frame(zod_name, zod_day)
 zod <- function(input) {
   valid_date <- dmy(input)
-  
-  
+  init_year <- year(valid_date)
+  init_date <- ymd(paste(init_year, 1, 1))
+  days_diff <- valid_date - init_date
+  paste('fecha inicio ', days_diff)
+  #select range
 }
 
 
+
 # Parte 4: Flights
+tiempo_vuelos <- flights
+tiempo_vuelos <- tiempo_vuelos[!is.na(tiempo_vuelos$dep_time), ]
+tiempo_vuelos <- tiempo_vuelos[!is.na(tiempo_vuelos$arr_time), ]
 
+tiempo_vuelos$dep_time_valid <- sprintf("%04d", tiempo_vuelos$dep_time)
+tiempo_vuelos$arr_time_valid <- sprintf("%04d", tiempo_vuelos$dep_time)
+tiempo_vuelos$sched_dep_time_valid <- sprintf("%04d", tiempo_vuelos$sched_dep_time)
+tiempo_vuelos$sched_arr_time_valid <- sprintf("%04d", tiempo_vuelos$sched_arr_time)
 
+tiempo_vuelos$valid_dep_time <- format(strptime(tiempo_vuelos$dep_time_valid, format="%H%M"), format = "%H:%M")
+tiempo_vuelos$valid_arr_time <- format(strptime(tiempo_vuelos$arr_time_valid, format="%H%M"), format = "%H:%M")
+tiempo_vuelos$valid_sched_dep_time <- format(strptime(tiempo_vuelos$sched_dep_time_valid, format="%H%M"), format = "%H:%M")
+tiempo_vuelos$valid_sche_arr_time <- format(strptime(tiempo_vuelos$sched_arr_time_valid, format="%H%M"), format = "%H:%M")
+
+tiempo_vuelos$Salida <- ymd_hm(paste(tiempo_vuelos$year, tiempo_vuelos$month, tiempo_vuelos$day, tiempo_vuelos$valid_dep_time))
+tiempo_vuelosLlegada <- ymd_hm(paste(tiempo_vuelos$year, tiempo_vuelos$month, tiempo_vuelos$day, tiempo_vuelos$valid_arr_time))
+tiempo_vuelos$Salida_estimada <- ymd_hm(paste(tiempo_vuelos$year, tiempo_vuelos$month, tiempo_vuelos$day, tiempo_vuelos$valid_sched_dep_time))
+tiempo_vuelos$llegada_estimada <- ymd_hm(paste(tiempo_vuelos$year, tiempo_vuelos$month, tiempo_vuelos$day, tiempo_vuelos$valid_sche_arr_time))
+tiempo_vuelos$Retraso <- tiempo_vuelos$dep_delay + tiempo_vuelos$arr_delay
